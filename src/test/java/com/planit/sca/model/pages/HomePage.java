221c171cab437d26ed92564c8030905c3b00e3df
Jason Xiao (Hidajat) <jaesonxh@gmail.com>
2022-05-26 11:19:25 +1000

    added navbar functionality to POM of homepage

--- src/test/java/com/planit/sca/model/pages/HomePage.java
+++ src/test/java/com/planit/sca/model/pages/HomePage.java
@@ -3,8 +3,12 @@ package com.planit.sca.model.pages;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage extends BasePage {
+    public NavigationBar navBar; // considered making this private, but then would need to make another method in HomePage for every method of navBar
 
     public HomePage(WebDriver driver) {
         super(driver);
+        navBar = new NavigationBar();
     }
+
+
 }
